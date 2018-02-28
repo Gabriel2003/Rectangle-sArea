476797b2262f29c13703cabcb633661ebe0fffc5
gahur1962 <gahur1962@7HFM6Y1.Student.UGDSB.ED>
2018-02-28 11:47:13 -0500

    This is my first commit

---
+++ src/area/of/a/recatangle/AreaOfARecatangle.java
@@ -0,0 +1,30 @@
+/*
+ * Gabriel Hurtado 
+ * Area of a rectangle 
+ * this program calculates the area of a rectangle 
+ * February 27th,2018 
+ */
+
+package area.of.a.recatangle;
+
+/**
+ *
+ * @author gahur1962
+ */
+public class AreaOfARecatangle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Calculating the area of a rectangle 
+        
+        double lenght = 4.8; 
+        double width = 5.7;
+        double area = 0.0; 
+        // algorithm to calculate the area of the rectangle: 
+        area = width * lenght; 
+        System.out.println("area of a rectangle" + area + "cm squared");
+    }
+    
+}
